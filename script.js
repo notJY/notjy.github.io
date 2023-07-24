ec39ebdff1351a77eab8f365b9f25296530ef647
notJY <123806420+notJY@users.noreply.github.com>
2023-07-25 02:24:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -35,9 +35,44 @@ page3btn.addEventListener("click", function () {
 const menuBtn = document.querySelector("#menu");
 menuBtn.addEventListener("click", toggleMenus);
 const menuItemsList = document.querySelector("nav ul");
+const menuShow = document.querySelector("#menuList.menuShow");
 function toggleMenus() { /*open and close menu*/
 	menuItemsList.classList.toggle("menuShow");
 	//if (menuItemsList.style.display == "block")
 	//	menuItemsList.style.display = "none";
 	//else menuItemsList.style.display = "block";
 }//can optimize using toggle class with css transitions
+
+console.log(window.innerWidth);
+
+/*If screen width <= 800px, hide the menu item list on page load*/
+if (window.innerWidth <= 800) {
+	document.addEventListener("DOMContentLoaded", toggleMenus);
+}
+
+/*For page 1*/
+/*Get all the buttons*/
+const leafButton = document.getElementById("leafButton");
+const branchButton = document.getElementById("branchButton");
+const trunkButton = document.getElementById("trunkButton");
+const rootButton = document.getElementById("rootButton");
+
+/*For showing the information when the player clicks on the buttons*/
+function toggleInfo(infoClass) {
+	/*Get the containers to toggle*/
+	var info = document.getElementsByClassName(infoClass);
+	/*loop twice because words and image are separate*/
+	for (var i = 0; i < 2; i++) {
+		if (info[i].style.display == "none") {
+			info[i].style.display = "block";
+		}
+		else {
+			info[i].style.display = "none";
+		}
+	}
+}
+
+leafButton.addEventListener("click", function () { toggleInfo("leaves"); });
+branchButton.addEventListener("click", function () { toggleInfo("branch"); });
+trunkButton.addEventListener("click", function () { toggleInfo("trunk"); });
+rootButton.addEventListener("click", function () { toggleInfo("roots"); });
